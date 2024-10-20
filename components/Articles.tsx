c011ff93f18b6fbc5a0df929c3d606454dbc0f3f
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2024-10-20 21:22:49 +0300

    Added Articles content

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -12,16 +12,22 @@ export default function Articles() {
       title: "Strangler Fig Pattern",
       description:
         "In today’s software world, there are many large, monolithic software projects. These projects can be hard to kee...",
+      endPoint:
+        "https://medium.com/@hacisimsek/strangler-fig-pattern-742f2f52f297",
     },
     {
       title: "Centralized Service Registry",
       description:
         "The world of microservices is growing and evolving with each passing day. If we were to mention one of the bigge...",
+      endPoint:
+        "https://medium.com/@hacisimsek/centralized-service-registry-15ea87b38d71",
     },
     {
       title: "Understanding Circuit Breaker Pattern in Microservices",
       description:
         "Imagine you are in an entertainment venue and sudden power surges threaten your systems. Fortunately, a small dev...",
+      endPoint:
+        "https://medium.com/@hacisimsek/what-is-the-circuit-breaker-pattern-and-why-is-it-important-for-microservice-architecture-6c997be77949",
     },
   ];
 
@@ -30,14 +36,18 @@ export default function Articles() {
       <h2 className="text-2xl font-bold mb-4">Latest Articles</h2>
       <div className="space-y-4">
         {articles.map((article, index) => (
-          <Card key={index}>
-            <CardHeader>
-              <CardTitle>{article.title}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <CardDescription>{article.description}</CardDescription>
-            </CardContent>
-          </Card>
+          <div key={index}>
+            <a href={article.endPoint} target="_blank" className="mb-12">
+              <Card>
+                <CardHeader>
+                  <CardTitle>{article.title}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <CardDescription>{article.description}</CardDescription>
+                </CardContent>
+              </Card>
+            </a>
+          </div>
         ))}
       </div>
     </section>
