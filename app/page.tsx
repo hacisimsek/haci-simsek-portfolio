9ca4cef2defbcd5e976df11f24f7fa8d92bbea21
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2024-11-05 15:15:47 +0300

    Remove unnecessary main tag from Home component for cleaner structure

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,9 @@ import Education from '@/components/Education';
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto px-4 py-8">
-      <main>
         <About />
         <Career />
         <Education />
-      </main>
     </div>
   );
 }
