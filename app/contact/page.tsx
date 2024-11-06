74082bf756a5636a7a62adfec6df6321c509418d
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2024-11-06 22:44:20 +0300

    Integrate Formspree for contact form handling and add validation errors

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,42 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useForm, ValidationError } from "@formspree/react";
 
 export default function Contact() {
+  const [state, handleSubmit, reset] = useForm("xanyoozp");
+  if (state.succeeded) {
+    return <div>Thank you for signing up!</div>;
+  }
   return (
-    <section id="contact" className="mb-12" >
+    <section id="contact" className="mb-12">
       <h4 className="text-xl font-bold mb-4">Contact Me</h4>
-      <form className="space-y-4" action="https://formspree.io/f/xanyoozp" method="POST">
-        <Input type="text" placeholder="Your Name" required aria-label="Your Name"/>
-        <Input type="email" placeholder="Your Email" required aria-label="Your Email"/>
-        <Textarea placeholder="Your Message" required aria-label="Your Message" rows={5}/>
-        <Button type="submit">Send Message</Button>
+      <form className="space-y-4" onSubmit={handleSubmit}>
+        <Input type="text" placeholder="Your Name" aria-label="Your Name" />
+        <Input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          aria-label="Your Email"
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <Textarea
+          id="message"
+          name="message"
+          placeholder="Your Message"
+          aria-label="Your Message"
+          rows={5}
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <Button type="submit" disabled={state.submitting}>
+          Send Message
+        </Button>
       </form>
     </section>
   );
