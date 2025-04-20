e81073692e4a105b38d18b4a81f38ee339c1c97a
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2025-04-20 15:21:26 +0300

    fix: improve chart ID generation by removing colons

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -44,7 +44,7 @@ const ChartContainer = React.forwardRef<
   }
 >(({ id, className, children, config, ...props }, ref) => {
   const uniqueId = React.useId();
-  const chartId = `chart-${id || uniqueId.replace(/:/g, '')}`;
+  const chartId = `chart-${(id || uniqueId).split(':').join('')}`;
 
   return (
     <ChartContext.Provider value={{ config }}>
